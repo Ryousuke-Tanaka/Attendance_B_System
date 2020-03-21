35fdb3b77a853228cd096bd1f951240b6fd700cf
Ryousuke-Tanaka <sample@email.com>
2020-03-21 14:32:49 +0000

    ユーザー新規作成createアクション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # paramsハッシュからユーザーを取得します。
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "新規ユーザー作成に成功しました。"
+      redirect_to "#"
+    else
+      render :new
+    end
   end
   
 end
