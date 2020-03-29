aa00d7655d297fbbf320c71f8e6c35ac2ea9a82f
Ryousuke-Tanaka <sample@email.com>
2020-03-29 06:03:38 +0000

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,34 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  
+  # beforeフィルター
+  
   # paramsハッシュからユーザーを取得します。
   def set_user
     @user = User.find(params[:id])
   end
   
-  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  # ログイン済みのユーザーか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+    
+  # アクセスしたユーザーが現在ログインしているユーザーか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+    
+  # 管理者か判定
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
   
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,31 +67,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:department, basic_time, :work_time)
     end
   
-    # beforeフィルター
-    
-    # paramsハッシュからユーザーを取得
-    def set_user
-      @user = User.find(params[:id])
-    end
-    
-    # ログイン済みのユーザーか確認
-    def logged_in_user
-      unless logged_in?
-      store_location
-      flash[:danger] = "ログインしてください。"
-      redirect_to login_url
-      end
-    end
-    
-    # アクセスしたユーザーが現在ログインしているユーザーか確認
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
-    end
-    
-    # 管理者か判定
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
-  
 end
