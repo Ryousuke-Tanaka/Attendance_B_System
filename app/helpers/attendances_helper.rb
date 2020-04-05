8c1ad0139cbb3b7633bfd3c30b76122c158b77d4
Ryousuke-Tanaka <sample@email.com>
2020-04-05 10:34:04 +0000

    完成20200405_02

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -6,12 +6,17 @@ module AttendancesHelper
       return "出社" if attendance.started_at.nil?
       return "退社" if attendance.started_at.present? && attendance.finished_at.nil?
     end
-    # どれにも当てはまらなかったらfalseを返します。
+    # どれにも当てはまらなかったらfalseを返す
     false
   end
   
-  # 出勤時間ち退勤時間を受け取り、在社時間を計算して返す
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返す
   def working_times(start, finish)
     format("%.2f",(((finish - start) / 60) / 60.0))
+  end
+  
+  # 時間を15分刻みで表示する
+  def set_minutes(time)
+    format("%.2d", time.floor_to(15.minutes).min)
   end
 end
